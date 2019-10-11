f4f29a48226227a5589374cee027116c5beb36f4
Francesco Salvadori <francesco.salvadori@gmail.com>
2019-10-11 08:16:58 +0200

    example_03 Extended with update property

--- example_03/index.js
+++ example_03/index.js
@@ -6,3 +6,10 @@ sheets.append(
         ["data 1", "data 2", "data3"]
     ]
 );
+
+sheets.update(
+    "Foglio1!A7",
+    [
+        ["UPDATE", "UPDATE", "UPDATE"]
+    ]
+);
--- example_03/sheets.js
+++ example_03/sheets.js
@@ -24,4 +24,25 @@ const append = (range, values) => {
         });
     });
 }
-module.exports = {append};
+
+const update = (range, values) => {
+    fs.readFile('credentials.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        // Authorize a client with credentials, then call the Google Sheets API.
+        authorize(JSON.parse(content), (auth) => {
+            const sheets = google.sheets({ version: 'v4', auth });
+            const valueInputOption = 'USER_ENTERED';
+            const resource = {values};
+            sheets.spreadsheets.values.update({
+                spreadsheetId, range, valueInputOption, resource
+            }, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Success!");
+                }
+            });
+        });
+    });
+}
+module.exports = {append, update};
